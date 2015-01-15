1cc3679577c73ae6ffd4c192b92d5718b3e8f195
Bent Cardan <bent@nothingsatisfies.com>
2015-01-15 15:33:15 -0500

    socket method

--- test/socket.open.js
+++ test/socket.open.js
@@ -5,54 +5,53 @@ var after   = require('after')
 
 describe('socket.open', function() {
 
-  var fam = nmsg.AF_SP
   it('should open NN_PUB', function (done) {
-    nmsg.Socket( fam, nmsg.NN_PUB).should.be.above(-1)
+    nmsg.socket('pub').should.be.above(-1)
     done()
   })
 
   it('should open NN_SUB', function (done) {
-    nmsg.Socket( fam, nmsg.NN_SUB).should.be.above(-1)
+    nmsg.socket('sub').should.be.above(-1)
     done()
   })
 
   it('should open NN_BUS', function (done) {
-    nmsg.Socket( fam, nmsg.NN_BUS).should.be.above(-1)
+    nmsg.socket('bus').should.be.above(-1)
     done()
   })
 
   it('should open NN_PAIR', function (done) {
-    nmsg.Socket( fam, nmsg.NN_PAIR).should.be.above(-1)
+    nmsg.socket('pair').should.be.above(-1)
     done()
   })
 
   it('should open NN_SURVEYOR', function (done) {
-    nmsg.Socket( fam, nmsg.NN_SURVEYOR).should.be.above(-1)
+    nmsg.socket('surveyor').should.be.above(-1)
     done()
   })
 
   it('should open NN_RESPONDENT', function (done) {
-    nmsg.Socket( fam, nmsg.NN_RESPONDENT).should.be.above(-1)
+    nmsg.socket('respondent').should.be.above(-1)
     done()
   })
 
   it('should open NN_REQ', function (done) {
-    nmsg.Socket( fam, nmsg.NN_REQ).should.be.above(-1)
+    nmsg.socket('req').should.be.above(-1)
     done()
   })
 
   it('should open NN_REP', function (done) {
-    nmsg.Socket( fam, nmsg.NN_REP).should.be.above(-1)
+    nmsg.socket('rep').should.be.above(-1)
     done()
   })
 
   it('should open NN_PULL', function (done) {
-    nmsg.Socket( fam, nmsg.NN_PULL).should.be.above(-1)
+    nmsg.socket('pull').should.be.above(-1)
     done()
   })
 
   it('should open NN_PUSH', function (done) {
-    nmsg.Socket( fam, nmsg.NN_PUSH).should.be.above(-1)
+    nmsg.socket('push').should.be.above(-1)
     done()
   })
 })
