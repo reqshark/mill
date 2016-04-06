d6b99ceaf8d1df7782018a06417f538a33a92c9b
Bent Cardan <bent@nothingsatisfies.com>
2016-04-06 10:32:57 -0400

    socket buffer integrity check

--- test/sodium.js
+++ test/sodium.js
@@ -3,6 +3,7 @@ module.exports  = sodium
 
 function sodium (t) {
   t.test('nbuf', nbuf)
+  t.test('socket buffer integrity check', socketbuffers)
 }
 
 function nbuf (t) {
@@ -12,4 +13,24 @@ function nbuf (t) {
 
   t.ok( nonce,   `nonce hex: ${nonce}` );
   t.is( nonce.length,  48, `nonce hex length: ${nonce.length}`   );
+}
+
+function socketbuffers (t) {
+  t.plan(20)
+
+  var port      = 5678
+  var i         = 20
+  var bufs      = {}
+  var sockets   = []
+
+  while (i--) {
+    var ipaddr = lib.iplocal(port++)
+    var s = lib.udplisten(ipaddr)
+    bufs[ s ] = i
+    sockets.push(s)
+  }
+
+  sockets
+    .reverse()
+    .map( s => t.is( ++i,  bufs[s], `socketbuffer # ${i} == ${bufs[s]}` ) )
 }
